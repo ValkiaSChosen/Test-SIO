f54573d0cfc875b218dcdc0df5b730b8b09e63c3
unknown <tangi.dekerdrel@gmail.com>
2023-06-06 15:57:29 +0200

    vue de tg

---
+++ views/view_git_tg.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <title>BTS-blanc</title>
+   <link rel="stylesheet" href="./views/public/css/style.css">
+</head>
+
+<body>
+   <?php include_once("./views/include/header.php") ?>
+   <?php include_once("./views/include/nav.php") ?>
+
+    <main>
+        <h2> Test Git </h2>
+    </main>
+   <?php include("./views/include/footer.php"); ?>
+
+</body>
+
+</html>
